dc7615e047b496edf3b39c5487ffc5e737da6156
Ildar Tukhvatullin <ildar59a@mail.ru>
2026-09-18 21:06:48 +0700

    feat: перенесены функции варианта 100, добавлены пункты меню 3-5

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 
 // Блок подключений: заголовочные файлы 
 #include "ubogov.h"
-//#include "tuhvatullin.h"
+#include "tuhvatullin.h"
 // Конец блока подключений 
 
 using namespace std;
@@ -19,6 +19,7 @@ int main() {
 
 	int choice;
 	double a, b;
+	double income, expense, rem, days;
 
 	do {
 		cout << "\n=== Командный проект: сборник расчетов ===\n";
@@ -26,6 +27,9 @@ int main() {
 		// === БЛОК МЕНЮ: каждый участник добавляет свои пункты ===
 		cout << "1. Площадь Эллипса\n";
 		cout << "2. Периметр Эллипса\n";
+		cout << "3. Доля расходов в доходе (%)\n";
+		cout << "4. Остаток от дохода\n";
+		cout << "5. Дневной бюджет\n";
 		// === КОНЕЦ БЛОКА МЕНЮ ===
 		cout << "0. Выход\n";
 		cout << "Выберите пункт меню: ";
@@ -43,6 +47,21 @@ int main() {
 			cout << "Введите размеры осей a и b в сантиметрах: ";
 			cin >> a >> b;
 			cout << "Периметр эллипса = " << ellipsePerimeter(a, b) << " см.\n";
+			break;
+		case 3:
+			cout << "Введите расход и доход: ";
+			cin >> expense >> income;
+			cout << "Доля расходов = " << sharePercent(expense, income) << "%\n";
+			break;
+		case 4:
+			cout << "Введите доход и расходы: ";
+			cin >> income >> expense;
+			cout << "Остаток = " << remainder(income, expense) << "\n";
+			break;
+		case 5:
+			cout << "Введите остаток и количество дней: ";
+			cin >> rem >> days;
+			cout << "Дневной бюджет = " << dailyBudget(rem, days) << "\n";
 			break;
 		default:
 			cout << "Такого пункта нет.\n";
---
+++ tuhvatullin.h
@@ -0,0 +1,8 @@
+#ifndef TUHVATULLIN_H
+#define TUHVATULLIN_H
+
+double sharePercent(double expense, double income);
+double remainder(double income, double expenses);
+double dailyBudget(double remainderValue, double days);
+
+#endif //TUHVATULLIN_H
